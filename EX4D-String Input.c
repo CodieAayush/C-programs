ea97e836c63eca0e3b5f251207bf0922df0ef057
CodieAayush <120035320+CodieAayush@users.noreply.github.com>
2023-01-19 21:25:16 +0530

    Create EX4D-String Input.c
    Using gets and puts function on string

---
+++ EX4D-String Input.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+    char s[10];
+    printf("Enter a string: ");
+    gets(s);
+    printf("your entered string is: ");
+    puts(s);
+    getch();
+}
